a49d944fd080a0329cd006e04d6f3f56d46df97b
Toshiyuki Masui <masui@pitecan.com>
2011-03-15 21:18:16 +0900

    WordSearchのメソッドリスト

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
-copy:
+install:
 	cp -r build/Debug/Gyaim.app ~/Library/Input\ Methods
+test:
+	ruby Tests/run_suite.rb
--- WordSearch/WordSearch.rb
+++ WordSearch/WordSearch.rb
@@ -1,8 +1,19 @@
+# -*- coding: utf-8 -*-
 # WordSearch.rb
 # Gyaim
 #
 # Created by Toshiyuki Masui on 11/03/15.
-# Copyright 2011 __MyCompanyName__. All rights reserved.
+# Copyright 2011 Pitecan Systems. All rights reserved.
 
 class WordSearch
+  attr :searchMode, true
+
+  def initialize
+  end
+
+  def search
+  
+  def candidates
+    ["漢字", "変換", "ですよ"]
+  end
 end
